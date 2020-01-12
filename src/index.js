47e0786283baff07beabea8fdef82666d932f96f
alan <alanarostegui@hotmail.com>
2020-01-12 15:12:24 -0600

    components login and register routes

--- src/index.js
+++ src/index.js
@@ -5,14 +5,18 @@ import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Login from './components/Auth/Login';
+import Register from './components/Auth/Register';
 
 const Root = () => (
     <Router>
         <Switch>
-            <Route exact path="/" component={App} />
+        <Route exact path="/" component={App} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
         </Switch>
     </Router>
 )
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
